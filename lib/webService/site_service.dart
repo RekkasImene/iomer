1b6040ad700c68700ca1170c7ea2a3d27a33d543
imene <gi_rekkas@esi.dz>
2021-12-16 10:50:10 +0100

    get sites from web service V1

--- lib/webService/site_service.dart
+++ lib/webService/site_service.dart
@@ -4,10 +4,11 @@ import 'dart:developer';
 import 'package:iomer/webService/site.dart';
 import 'package:http/http.dart' as http;
 
-var url = 'https://silent-fireant-91.loca.lt/datasnap/rest/TServerMethodsIOmere/GetSites';
+var url = 'https://40f4-193-52-159-57.ngrok.io';
 
 Future<Site> fetchSite() async {
-  final response = await http.get(Uri.parse(url));
+  final response = await http
+      .get(Uri.parse('$url/datasnap/rest/TServerMethodsIOmere/GetSites'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,

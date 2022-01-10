fe3233308f235d46f19bf80f1cf7540f9d5aa9e3
Amel CHERIFI <amel.cherifi@univ-rouen.fr>
2022-01-10 11:49:01 +0100

    InterdSelection de la machine

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
-import 'dart:async';
-import 'package:drift/native.dart';
+
 import 'package:flutter/material.dart';
-import 'package:iomer/models/bdd/iomer_database.dart';
-import 'package:iomer/webService/categories.dart';
-import 'package:iomer/webService/equipements.dart';
-import 'package:iomer/webService/matricules.dart';
-import 'package:iomer/webService/origines.dart';
-import 'package:iomer/webService/ot_taches.dart';
-import 'package:iomer/webService/ots.dart';
-import 'package:iomer/webService/services.dart';
-import 'package:iomer/webService/sites.dart';
+
+
+import 'package:iomer/ui/select_machine.dart';
+
 
 void main() {
-
   runApp(const MyApp());
 }
 
@@ -24,64 +17,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<Sites> futureSite;
-  late Future<Origines> futureOrigines;
-  late Future<Matricules> futureMatricules;
-  late Future<Equipements> futureEquipements;
-  late Future<Categories> futureCategories;
-  late Future<OTs> futureOTs;
-  late Future<OTTaches> futureOTTaches;
+  
   @override
   void initState() {
     super.initState();
-    int idSite =2;
-    int idOrigine =21;
-    int idOT=203;
-    futureSite = fetchSite();
-    futureOrigines = fetchOrigines(idSite);
-    futureMatricules=fetchMatricules(idOrigine);
-    futureEquipements=fetchEquipements(idSite);
-    futureCategories=fetchCategories(idSite);
-    futureOTs=fetchOTs(idSite, idOrigine);
-    futureOTTaches=fetchOTTaches(idOT);
+    
 
-    IomerDatabase database;
-    database = IomerDatabase();
-    final site = Site(
- nomSite: 'azerty', codeSite: 'zero', adresseSite: 'klklkl', idSite: 1,
-    );
-
-    database.insertSite(site);
+  
   }
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Fetch Data Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<Sites>(
-            future: futureSite,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.nomsite);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
+      title: 'IOmere',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const SelectMachine(),
     );
   }
 }

ca41b07c61e883c6b2facd2fc5a017e0c5eff507
rekkasimene <gi_rekkas@esi.dz>
2022-01-27 13:15:29 +0100

    regler un conflit rond 4

--- lib/config/injection.config.dart
+++ lib/config/injection.config.dart
@@ -7,12 +7,12 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../bloc/matricule/matricule_bloc.dart' as _i5;
+import '../bloc/matricule/matricule_bloc.dart' as _i4;
 import '../bloc/ot/ot_bloc.dart' as _i6;
 import '../bloc/site/sites_bloc.dart' as _i9;
 import '../models/bdd/iomer_database.dart' as _i3;
 import '../models/repository/in_repository.dart' as _i8;
-import '../models/repository/local_repository.dart' as _i4;
+import '../models/repository/local_repository.dart' as _i5;
 import '../models/repository/out_repository.dart' as _i7;
 
 const String _prod = 'prod';
@@ -22,23 +22,19 @@ const String _prod = 'prod';
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.factory<_i3.IomerDatabase>(() => _i3.IomerDatabase(),
+  gh.singleton<_i3.IomerDatabase>(_i3.IomerDatabase(), registerFor: {_prod});
+  gh.factory<_i4.MatriculeBloc>(
+      () => _i4.MatriculeBloc(get<_i5.LocalRepository>()),
       registerFor: {_prod});
-  gh.singleton<_i4.LocalRepository>(
-      _i4.LocalRepository(get<_i3.IomerDatabase>()),
-      registerFor: {_prod});
-  gh.factory<_i5.MatriculeBloc>(
-      () => _i5.MatriculeBloc(get<_i4.LocalRepository>()),
-      registerFor: {_prod});
-  gh.factory<_i6.OtBloc>(() => _i6.OtBloc(get<_i4.LocalRepository>()),
+  gh.factory<_i6.OtBloc>(() => _i6.OtBloc(get<_i5.LocalRepository>()),
       registerFor: {_prod});
   gh.singleton<_i7.OutRepository>(_i7.OutRepository(get<_i3.IomerDatabase>()),
       registerFor: {_prod});
   gh.singleton<_i8.InRepository>(
-      _i8.InRepository(get<_i3.IomerDatabase>(), get<_i4.LocalRepository>()),
+      _i8.InRepository(get<_i3.IomerDatabase>(), get<_i5.LocalRepository>()),
       registerFor: {_prod});
   gh.factory<_i9.SitesBloc>(
-      () => _i9.SitesBloc(get<_i8.InRepository>(), get<_i4.LocalRepository>()),
+      () => _i9.SitesBloc(get<_i8.InRepository>(), get<_i5.LocalRepository>()),
       registerFor: {_prod});
   return get;
 }

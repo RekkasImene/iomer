5c346a8f6745bde53b7e7c41048cbea5cfadb9db
prosper edah <prospernews97etude@gmail.com>
2022-01-27 13:49:06 +0100

    clean code and add delete database method and update methode

---
+++ lib/models/bdd/generate/config.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../config.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$ConfigDaoMixin on DatabaseAccessor<IomerDatabase> {
+  $ConfigsTable get configs => attachedDatabase.configs;
+}
---
+++ lib/models/bdd/generate/ot.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../ot.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$OtDaoMixin on DatabaseAccessor<IomerDatabase> {
+  $OtsTable get ots => attachedDatabase.ots;
+}

aef47d82c404dae2cafe9aba2e36a4d37a95cbf8
Bryan <barrebryan289@gmail.com>
2022-02-24 09:16:09 +0100

    ui border corrected

--- lib/ui/parts/components/list_parts.dart
+++ lib/ui/parts/components/list_parts.dart
@@ -32,41 +32,42 @@ class _ListPartsState extends State<ListParts> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-      child: BlocProvider<PartsBloc>(
-        create: (context) => _partsBloc,
-        child: BlocConsumer<PartsBloc, PartsState>(
-            listener: (context, state) {
+    return BlocProvider<PartsBloc>(
+      create: (context) => _partsBloc,
+      child: BlocConsumer<PartsBloc, PartsState>(
+          listener: (context, state) {
 
-              if (state is PartsLoaded) {
-                for (int i = 0; i < state.reservation.length; i++) {
-                  _controller.add(TextEditingController());
-                  _controller[i].text =state.reservation[i].QTEARTICLE.toString();
-                }
+            if (state is PartsLoaded) {
+              for (int i = 0; i < state.reservation.length; i++) {
+                _controller.add(TextEditingController());
+                _controller[i].text =state.reservation[i].QTEARTICLE.toString();
               }
+            }
 
-              if (state is StatePartsInternetOk) {
-                print("Navigation vers NewPartsScreen");
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const NewPartScreen())).then(onGoBack);
-              }
+            if (state is StatePartsInternetOk) {
+              print("Navigation vers NewPartsScreen");
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const NewPartScreen())).then(onGoBack);
+            }
 
-              if(state is StatePartsInternetError) {
-                showToast(state.message);
-                _partsBloc.add(FetchEventParts());
-              }
+            if(state is StatePartsInternetError) {
+              showToast(state.message);
+              _partsBloc.add(FetchEventParts());
+            }
 
-              if (state is StatePartsNoArticle) {
-                showToast(state.message);
-              }
-            },
+            if (state is StatePartsNoArticle) {
+              showToast(state.message);
+            }
+          },
 
-            builder: (context, state) {
-              print(state.toString());
-              if (state is PartsLoaded) {
-                return Column(
-                  children: [
-                    Expanded(
+          builder: (context, state) {
+            print(state.toString());
+            if (state is PartsLoaded) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+
                       child: ListView.builder(
                         itemCount: state.reservation.length,
                         itemBuilder: (context, index) {
@@ -112,48 +113,48 @@ class _ListPartsState extends State<ListParts> {
                         },
                       ),
                     ),
+                  ),
 
-                    Row(
-                      children: [
-                        const Expanded(child: SizedBox()),
-                        Align(
-                          child: ElevatedButton(
-                            onPressed: () {
-                              _partsBloc.add(InternetEventParts());
-                            },
-                            child: const Text('Ajout'),
-                            style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 10, vertical: 10)),
-                          ),
+                  Row(
+                    children: [
+                      const Expanded(child: SizedBox()),
+                      Align(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            _partsBloc.add(InternetEventParts());
+                          },
+                          child: const Text('Ajout'),
+                          style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 10)),
                         ),
-                      ],
-                    ),
-
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          [
-                            _partsBloc.add(UpdateEventListParts(state.reservation, _controller)),
-                            Navigator.pop(context),
-                          ];
-                        },
-                        child: const Text('Valider', style: TextStyle(fontSize: 20)),
-                        style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20)),
                       ),
-                    )
-                  ],
-                );
-              } else if (state is PartsError) {
-                return Text(state.message);
-              }
-              return const Center(
-                child: SizedBox(
-                    width: 32, height: 32, child: CircularProgressIndicator()),
+                    ],
+                  ),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        [
+                          _partsBloc.add(UpdateEventListParts(state.reservation, _controller)),
+                          Navigator.pop(context),
+                        ];
+                      },
+                      child: const Text('Valider', style: TextStyle(fontSize: 20)),
+                      style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20)),
+                    ),
+                  )
+                ],
               );
-            }),
-      ),
+            } else if (state is PartsError) {
+              return Text(state.message);
+            }
+            return const Center(
+              child: SizedBox(
+                  width: 32, height: 32, child: CircularProgressIndicator()),
+            );
+          }),
     );
   }
 
